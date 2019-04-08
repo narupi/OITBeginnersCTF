4d8693f3bdb2ab87d5559179a5d4a61f39097455
narupi <narupi1999@gmail.com>
2019-04-08 22:56:05 +0900

    first commit

---
+++ Reversing/rev1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void){
+
+    char buffer[128];
+    size_t length;
+
+    printf("Authentication required\n");
+    printf("Who are you?\n");
+
+    if (fgets(buffer, 128, stdin) == NULL) {
+        return 1;
+    }
+
+    length = strlen(buffer);
+    if (length > 0 && buffer[length - 1] == '\n') {
+        buffer[--length] = '\0';
+    }
+
+    if ( strcmp( buffer, "adm!n!strat0r" ) == 0 ){
+        printf("Authentication complete!\n");
+        printf("Flag is : oitctf{eazy_b1nary!}\n");
+    }
+    else {
+        printf("Authentication failed\n");
+    }
+        
+    return 0;
+}
+
