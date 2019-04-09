d668b9c862c2d6b538179d3e8be5df3e88073485
narupi <narupi1999@gmail.com>
2019-04-09 13:15:41 +0900

    update

---
+++ Reversing/src/rev3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int calc(void){
+    int ans = 1;
+    int i;
+
+    for(i = 0; i < 8; i++){
+        ans *= 2;
+    }
+    return ans;
+
+}
+
+
+int main(void) {
+    int flag;
+    flag = calc();
+    printf("Calculation finished\n");
+}
